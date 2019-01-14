00cd14c927c2bd68952e2d0d35b4ff85a2373a52
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-14 13:55:25 +0100

    Delete skript-2019-01-11-31.ps1

--- skript-2019-01-11-31.ps1
+++
@@ -1,6 +0,0 @@
-﻿write-host "Hello World"
-
-write-host -backgroundcolor blue -foregroundcolor yellow "Hello World"
-
-help clear-host -online
-#Tar en till microsoft-hjälp sida i ett nytt webfönster.
