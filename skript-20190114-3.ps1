235d16ba7a2edf68bb1ea40b84dfb11938c7f1bd
gustavlowenhoff <46317605+gustavlowenhoff@users.noreply.github.com>
2019-01-14 13:56:04 +0100

    Delete skript-20190114-3.ps1

--- skript-20190114-3.ps1
+++
@@ -1,7 +0,0 @@
-﻿New-Item -Path C:\temp\test2.txt -ItemType file
-
-Add-Content c:\temp\test2.txt $(get-date -displayHint time)
-
-add-Content c:\temp\test2.txt "Välkommen hem"
-
-Compare-Object -ReferenceObject $(Get-Content C:\temp\test.txt) -DifferenceObject $(Get-Content C:\temp\test2.txt)
